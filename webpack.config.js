dd65c64cbca326f8564e445fd895735650474ddd
Henri Bourcereau <henri.bourcereau@gmail.com>
2016-05-02 21:22:22 +0200

    library: node & browser versions

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/cssantique',
+  entry: './cssantique',
   output: {
     path: path.resolve(__dirname),
-    filename: 'cssantique.js',
+    filename: 'cssantique-browser.js',
     libraryTarget: 'var',
     library: 'CSSAntique'
   },
   module: {
     loaders: [
-      { test: /.json/, include: [path.resolve(__dirname, 'node_modules/browser-data/')], loader: 'json-loader' },
-      { test: /.js/, include: [path.resolve(__dirname, 'src')], loader: 'babel-loader' },
+      { test: /\.json/, include: [path.resolve(__dirname, 'node_modules/browser-data/')], loader: 'json-loader' },
+      { test: /\.js$/, include: [path.resolve(__dirname)], loader: 'babel-loader' },
     ]
   },
   node: {
